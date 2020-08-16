4ec1cba67761a32e64e1bdb6b26d87da7aef7f7a
Eniola <boluwatifearowoshere@gmail.com>
2020-08-16 01:22:43 -0700

    edit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// const header = document.querySelector('header');
+
+// document.addEventListener('scroll', () => {
+// if (window.pageYOffset == 85) {
+//     console.log(header);
+//     header.classList.add('hidden');
+//   } else if (window.pageYOffset >= 120) {
+//     console.log(header);
+//     header.classList.remove('hidden')
+//     header.classList.add('fixed');
+//   } else {
+//     header.classList.remove('fixed')
+//     header.classList.remove('fixed', 'hidden')
+//   }
+// });
